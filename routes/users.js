560f8608e5e7d91fe6e9c7890a7ccba6897d1d80
Aiden <15000487124@163.com>
2018-09-25 15:55:02 +0800

    新增用户提交记录接口 console.log增加时间

--- routes/users.js
+++ routes/users.js
@@ -37,8 +37,8 @@ router.post('/register', async (ctx, next) => {
         }
     }, function(error){
         // error
-        console.log("用户注册报错");
-        console.log(error);
+        console.log("用户注册报错",new Date().toLocaleString());
+        console.log(error,new Date().toLocaleString());
     });
     // 用户名已注册
     if(flag === 1){
@@ -49,16 +49,16 @@ router.post('/register', async (ctx, next) => {
     } else {
         // 用户名未注册
         await client.query("INSERT INTO user (userName,password) VALUES (?,?);", [data.username,data.password]).then(function(result) {
-            console.log("注册成功",data.username,data.password);
-            console.log(result);
+            console.log("注册成功",data.username,data.password,new Date().toLocaleString());
+            console.log(result,new Date().toLocaleString());
                 ctx.body = {
                     returnCode:'000000',
                     errMessage:'注册成功'
                 };
         }, function(error){
             // error
-            console.log("用户注册后台报错");
-            console.log(error);
+            console.log("用户注册后台报错",new Date().toLocaleString());
+            console.log(error,new Date().toLocaleString());
             ctx.body = {
                 returnCode:'999999',
                 errMessage:'注册失败，请检查网络'
@@ -75,9 +75,9 @@ router.post('/login', async (ctx, next) => {
     let userName = "";
     console.log("登录信息",data,data.password,new Date().toLocaleString());
     await client.query("select * from user where userName = ? and password = ?;", [data.username,data.password]).then(function(result) {
-        console.log('查询结果',result);
+        console.log('查询结果',result,new Date().toLocaleString());
         if(result.length === 0){
-            console.log("登录失败，账号密码不正确",data.username,data.password);
+            console.log("登录失败，账号密码不正确",data.username,data.password,new Date().toLocaleString());
             ctx.body = {
                 returnCode:'999999',
                 errMessage:'账号或密码错误'
@@ -89,8 +89,8 @@ router.post('/login', async (ctx, next) => {
         }
     }, function(error){
         // error
-        console.log("用户登录失败，数据库报错");
-        console.log(error);
+        console.log("用户登录失败，数据库报错",new Date().toLocaleString());
+        console.log(error,new Date().toLocaleString());
         ctx.body = {
             returnCode:'999999',
             errMessage:'登录失败，请检查网络'
@@ -100,17 +100,17 @@ router.post('/login', async (ctx, next) => {
     // 账号密码正确时
     if(flag){
         // 生成token
-        let result = createToken(userId,userName);
-        await client.query("update user set saveTime = ?, maxTime = ?, token = ? where userId = ?;", [result.saveTime,result.maxTime,result.token,userId]).then(function(result) {
-            console.log("插入结果",result);
+        let token = createToken(userId,userName);
+        await client.query("update user set saveTime = ?, maxTime = ?, token = ? where userId = ?;", [token.saveTime,token.maxTime,token.token,userId]).then(function(result) {
+            console.log("插入结果",result,new Date().toLocaleString());
         }, function(error){
             // error
-            console.log(error);
+            console.log(error,new Date().toLocaleString());
         });
         // 用cookie保存用户的登录状态
         ctx.cookies.set(
             'token',
-            result.token,
+            token.token,
             {
                 domain: frontServer,  // 写cookie所在的域名
                 path: '/',       // 写cookie所在的路径
@@ -127,6 +127,97 @@ router.post('/login', async (ctx, next) => {
             message:'登录成功'
         };
     }
+});
+
+// 用户提交记录
+router.post('/submitLog', async (ctx, next) => {
+    let data = ctx.request.body.params;
+    let token = ctx.cookies.get('token');
+    let flag = false; //判断数据库内是否有对应token
+    let userId = ''; //用户id
+    let userName = ''; //用户名
+    console.log('test data',data,new Date().toLocaleString());
+    console.log('test token',token,new Date().toLocaleString());
+    // 判断浏览器的cookie中是否存在token字段
+    if(typeof(token) === 'undefined'){
+        // 没有token时
+        ctx.body = {
+            returnCode:'999999',
+            errMessage:'登录超时，请重新登录',
+            goRouter:'/account/login'
+        };
+    } else {
+        // 有token时查库，判断库中是否有一致的token
+        await client.query("select * from user where token = ?;", [token]).then(function(result) {
+            console.log('token查询结果',result,new Date().toLocaleString());
+            if(result.length === 0){
+                // 数据库中没有符合的token
+                console.log("数据库中没有对应的token",token,new Date().toLocaleString());
+                ctx.body = {
+                    returnCode:'999999',
+                    errMessage:'登录超时，请重新登录',
+                    goRouter:'/account/login'
+                };
+            } else {
+                // 数据库中有对应的token
+                flag = true; //判断数据库内是否有对应token
+                userId = result[0].userId; //用户id
+                userName = result[0].userName; //用户名
+            }
+        }, function(error){
+            // error
+            console.log("用户token免登录失败，数据库报错",new Date().toLocaleString());
+            console.log(error,new Date().toLocaleString());
+            ctx.body = {
+                returnCode:'999999',
+                errMessage:'登录超时，请重新登录',
+                goRouter:'/account/login'
+            };
+        });
+    }
+    // 数据库中有对应的token时
+    if(flag){
+        // 生成token
+        let token = createToken(userId,userName);
+        console.log('新改的token',token.token);
+        // 更新用户的token
+        await client.query("update user set saveTime = ?, maxTime = ?, token = ? where userId = ?;", [token.saveTime,token.maxTime,token.token,userId]).then(function(result) {
+            console.log("用户token更新结果",result,new Date().toLocaleString());
+            // 用cookie保存用户的登录状态
+            ctx.cookies.set(
+                'token',
+                token.token,
+                {
+                    domain: frontServer,  // 写cookie所在的域名
+                    path: '/',       // 写cookie所在的路径
+                    maxAge: 10 * 60 * 1000, // cookie有效时长
+                    expires: new Date('2018-11-15'),  // cookie失效时间
+                    httpOnly: false,  // 是否只用于http请求中获取
+                    overwrite: true  // 是否允许重写
+                }
+            );
+        }, function(error){
+            // error
+            console.log(error,new Date().toLocaleString());
+        });
+        // 将用户提交的记录写入数据库
+        await client.query("INSERT INTO log (userId,category,categoryName,money,remarks) VALUES (?,?,?,?,?);", [userId,data.category,data.categoryName,data.money,data.remarks]).then(function(result) {
+            console.log("新增记录结果",result,new Date().toLocaleString());
+            ctx.body = {
+                returnCode:'000000',
+                message:'提交成功'
+            };
+        }, function(error){
+            // error
+            console.log(error,new Date().toLocaleString());
+            ctx.body = {
+                returnCode:'999999',
+                errMessage:'网络异常，请重新尝试',
+            };
+        });
+    }
+
+
 });
 
 module.exports = router
